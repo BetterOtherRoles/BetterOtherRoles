fa3fb943dac8fe6ea38d06afa00cfb36c056d956
Eno9991 <marwane.rhaiem@gmail.com>
2023-05-10 17:21:21 +0200

    Update v1.2.1

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -23,189 +23,189 @@ using TheOtherRoles.EnoFw;
 using TheOtherRoles.EnoFw.Kernel;
 using TheOtherRoles.Patches;
 
-namespace TheOtherRoles
+namespace TheOtherRoles;
+
+[BepInPlugin(Id, "The Other Roles", VersionString)]
+[BepInDependency(SubmergedCompatibility.SUBMERGED_GUID, BepInDependency.DependencyFlags.SoftDependency)]
+[BepInProcess("Among Us.exe")]
+[ReactorModFlags(Reactor.Networking.ModFlags.RequireOnAllClients)]
+public class TheOtherRolesPlugin : BasePlugin
 {
-    [BepInPlugin(Id, "The Other Roles", VersionString)]
-    [BepInDependency(SubmergedCompatibility.SUBMERGED_GUID, BepInDependency.DependencyFlags.SoftDependency)]
-    [BepInProcess("Among Us.exe")]
-    [ReactorModFlags(Reactor.Networking.ModFlags.RequireOnAllClients)]
-    public class TheOtherRolesPlugin : BasePlugin
-    {
-        public const string Id = "me.eisbison.theotherroles";
-        public const string VersionString = "1.2.0";
+    public const string Id = "me.eisbison.theotherroles";
+    public const string VersionString = "1.2.1";
 
-        public static Version Version = Version.Parse(VersionString);
-        internal static BepInEx.Logging.ManualLogSource Logger;
+    public static Version Version = Version.Parse(VersionString);
+    internal static BepInEx.Logging.ManualLogSource Logger;
          
-        public Harmony Harmony { get; } = new(Id);
-        public static TheOtherRolesPlugin Instance;
+    public Harmony Harmony { get; } = new(Id);
+    public static TheOtherRolesPlugin Instance;
 
-        public static int optionsPage = 2;
-
-        public static ConfigEntry<string> DebugMode { get; private set; }
-        public static ConfigEntry<bool> GhostsSeeInformation { get; set; }
-        public static ConfigEntry<bool> GhostsSeeRoles { get; set; }
-        public static ConfigEntry<bool> GhostsSeeModifier { get; set; }
-        public static ConfigEntry<bool> GhostsSeeVotes{ get; set; }
-        public static ConfigEntry<bool> ShowRoleSummary { get; set; }
-        public static ConfigEntry<bool> ShowLighterDarker { get; set; }
-        public static ConfigEntry<bool> EnableSoundEffects { get; set; }
-        public static ConfigEntry<bool> EnableHorseMode { get; set; }
-        public static ConfigEntry<string> Ip { get; set; }
-        public static ConfigEntry<ushort> Port { get; set; }
-        public static ConfigEntry<string> ShowPopUpVersion { get; set; }
-        public static ConfigEntry<string> DevGuid { get; set; }
-        
-        public static ConfigEntry<string> FeaturesCodes { get; private set; }
-
-        public static Sprite ModStamp;
-
-        public static IRegionInfo[] defaultRegions;
-
-
-        // This is part of the Mini.RegionInstaller, Licensed under GPLv3
-        // file="RegionInstallPlugin.cs" company="miniduikboot">
-        public static void UpdateRegions() {
-            ServerManager serverManager = FastDestroyableSingleton<ServerManager>.Instance;
-            var regions = new IRegionInfo[] {
-                new StaticHttpRegionInfo("Custom", StringNames.NoTranslation, Ip.Value, new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1] { new ServerInfo("Custom", Ip.Value, Port.Value, false) })).CastFast<IRegionInfo>()
-            };
-            
-            IRegionInfo currentRegion = serverManager.CurrentRegion;
-            Logger.LogInfo($"Adding {regions.Length} regions");
-            foreach (IRegionInfo region in regions) {
-                if (region == null) 
-                    Logger.LogError("Could not add region");
-                else {
-                    if (currentRegion != null && region.Name.Equals(currentRegion.Name, StringComparison.OrdinalIgnoreCase)) 
-                        currentRegion = region;               
-                    serverManager.AddOrUpdateRegion(region);
-                }
-            }
-
-            // AU remembers the previous region that was set, so we need to restore it
-            if (currentRegion != null) {
-                Logger.LogDebug("Resetting previous region");
-                serverManager.SetRegion(currentRegion);
-            }
-        }
-
-        public override void Load() {
-            Logger = Log;
-            Instance = this;
-
-            DebugMode = Config.Bind("Custom", "Enable Debug Mode", "false");
-            GhostsSeeInformation = Config.Bind("Custom", "Ghosts See Remaining Tasks", true);
-            GhostsSeeRoles = Config.Bind("Custom", "Ghosts See Roles", true);
-            GhostsSeeModifier = Config.Bind("Custom", "Ghosts See Modifier", true);
-            GhostsSeeVotes = Config.Bind("Custom", "Ghosts See Votes", true);
-            ShowRoleSummary = Config.Bind("Custom", "Show Role Summary", true);
-            ShowLighterDarker = Config.Bind("Custom", "Show Lighter / Darker", true);
-            EnableSoundEffects = Config.Bind("Custom", "Enable Sound Effects", true);
-            EnableHorseMode = Config.Bind("Custom", "Enable Horse Mode", false);
-            ShowPopUpVersion = Config.Bind("Custom", "Show PopUp", "0");
-            
-            Ip = Config.Bind("Custom", "Custom Server IP", "127.0.0.1");
-            Port = Config.Bind("Custom", "Custom Server Port", (ushort)22023);
-
-            defaultRegions = ServerManager.DefaultRegions;
-
-            UpdateRegions();
-            
-            DevGuid = Config.Bind("Custom", "Dev Guid", "");
-            FeaturesCodes = Config.Bind("Custom", "Feature codes", "");
-            Harmony.PatchAll();
-            
-            Customs.Load();
-            CustomColors.Load();
-
-            if (BepInExUpdater.UpdateRequired)
-            {
-                AddComponent<BepInExUpdater>();
-                return;
-            }
-
-            EventUtility.Load();
-
-            SubmergedCompatibility.Initialize();
-            AddComponent<ModUpdateBehaviour>();
-            MainMenuPatch.addSceneChangeCallbacks();
-            
-            System.Console.WriteLine($"Current GUID: {CustomGuid.CurrentGuid.ToString()}");
-        }
-        public static Sprite GetModStamp() {
-            if (ModStamp) return ModStamp;
-            return ModStamp = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.ModStamp.png", 150f);
-        }
-    }
-
-    // Deactivate bans, since I always leave my local testing game and ban myself
-    [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.AmBanned), MethodType.Getter)]
-    public static class AmBannedPatch
-    {
-        public static void Postfix(out bool __result)
-        {
-            __result = false;
-        }
-    }
-    [HarmonyPatch(typeof(ChatController), nameof(ChatController.Awake))]
-    public static class ChatControllerAwakePatch {
-        private static void Prefix() {
-            if (!EOSManager.Instance.isKWSMinor) {
-                DataManager.Settings.Multiplayer.ChatMode = InnerNet.QuickChatModes.FreeChatOrQuickChat;
-            }
-        }
-    }
+    public static int optionsPage = 2;
     
-    // Debugging tools
-    [HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
-    public static class DebugManager
+    public static ConfigEntry<int> Preset { get; private set; }
+
+    public static ConfigEntry<string> DebugMode { get; private set; }
+    public static ConfigEntry<bool> GhostsSeeInformation { get; set; }
+    public static ConfigEntry<bool> GhostsSeeRoles { get; set; }
+    public static ConfigEntry<bool> GhostsSeeModifier { get; set; }
+    public static ConfigEntry<bool> GhostsSeeVotes{ get; set; }
+    public static ConfigEntry<bool> ShowRoleSummary { get; set; }
+    public static ConfigEntry<bool> ShowLighterDarker { get; set; }
+    public static ConfigEntry<bool> EnableSoundEffects { get; set; }
+    public static ConfigEntry<bool> EnableHorseMode { get; set; }
+    public static ConfigEntry<string> Ip { get; set; }
+    public static ConfigEntry<ushort> Port { get; set; }
+    public static ConfigEntry<string> ShowPopUpVersion { get; set; }
+    public static ConfigEntry<string> DevGuid { get; set; }
+        
+    public static ConfigEntry<string> FeaturesCodes { get; private set; }
+
+    public static Sprite ModStamp;
+
+    public static IRegionInfo[] defaultRegions;
+
+
+    // This is part of the Mini.RegionInstaller, Licensed under GPLv3
+    // file="RegionInstallPlugin.cs" company="miniduikboot">
+    public static void UpdateRegions() {
+        ServerManager serverManager = FastDestroyableSingleton<ServerManager>.Instance;
+        var regions = new IRegionInfo[] {
+            new StaticHttpRegionInfo("Custom", StringNames.NoTranslation, Ip.Value, new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1] { new ServerInfo("Custom", Ip.Value, Port.Value, false) })).CastFast<IRegionInfo>()
+        };
+            
+        IRegionInfo currentRegion = serverManager.CurrentRegion;
+        Logger.LogInfo($"Adding {regions.Length} regions");
+        foreach (IRegionInfo region in regions) {
+            if (region == null) 
+                Logger.LogError("Could not add region");
+            else {
+                if (currentRegion != null && region.Name.Equals(currentRegion.Name, StringComparison.OrdinalIgnoreCase)) 
+                    currentRegion = region;               
+                serverManager.AddOrUpdateRegion(region);
+            }
+        }
+
+        // AU remembers the previous region that was set, so we need to restore it
+        if (currentRegion != null) {
+            Logger.LogDebug("Resetting previous region");
+            serverManager.SetRegion(currentRegion);
+        }
+    }
+
+    public override void Load() {
+        Logger = Log;
+        Instance = this;
+
+        DebugMode = Config.Bind("Custom", "Enable Debug Mode", "false");
+        GhostsSeeInformation = Config.Bind("Custom", "Ghosts See Remaining Tasks", true);
+        GhostsSeeRoles = Config.Bind("Custom", "Ghosts See Roles", true);
+        GhostsSeeModifier = Config.Bind("Custom", "Ghosts See Modifier", true);
+        GhostsSeeVotes = Config.Bind("Custom", "Ghosts See Votes", true);
+        ShowRoleSummary = Config.Bind("Custom", "Show Role Summary", true);
+        ShowLighterDarker = Config.Bind("Custom", "Show Lighter / Darker", true);
+        EnableSoundEffects = Config.Bind("Custom", "Enable Sound Effects", true);
+        EnableHorseMode = Config.Bind("Custom", "Enable Horse Mode", false);
+        ShowPopUpVersion = Config.Bind("Custom", "Show PopUp", "0");
+            
+        Ip = Config.Bind("Custom", "Custom Server IP", "127.0.0.1");
+        Port = Config.Bind("Custom", "Custom Server Port", (ushort)22023);
+
+        defaultRegions = ServerManager.DefaultRegions;
+
+        UpdateRegions();
+            
+        DevGuid = Config.Bind("Custom", "Dev Guid", "");
+        FeaturesCodes = Config.Bind("Custom", "Feature codes", "");
+        Preset = Config.Bind("MainConfig", "Preset", 0);
+        Harmony.PatchAll();
+        
+        CustomColors.Load();
+        CustomOptions.Load();
+
+        if (BepInExUpdater.UpdateRequired)
+        {
+            AddComponent<BepInExUpdater>();
+            return;
+        }
+
+        EventUtility.Load();
+
+        SubmergedCompatibility.Initialize();
+        AddComponent<ModUpdateBehaviour>();
+        MainMenuPatch.addSceneChangeCallbacks();
+            
+        System.Console.WriteLine($"Current GUID: {CustomGuid.CurrentGuid.ToString()}");
+        TheOtherRoles.LoadRoles();
+        CustomGuid.FetchAdmins();
+    }
+}
+
+// Deactivate bans, since I always leave my local testing game and ban myself
+[HarmonyPatch(typeof(StatsManager), nameof(StatsManager.AmBanned), MethodType.Getter)]
+public static class AmBannedPatch
+{
+    public static void Postfix(out bool __result)
     {
-        private static readonly string passwordHash = "d1f51dfdfd8d38027fd2ca9dfeb299399b5bdee58e6c0b3b5e9a45cd4e502848";
-        private static readonly System.Random random = new System.Random((int)DateTime.Now.Ticks);
-        private static List<PlayerControl> bots = new List<PlayerControl>();
+        __result = false;
+    }
+}
+[HarmonyPatch(typeof(ChatController), nameof(ChatController.Awake))]
+public static class ChatControllerAwakePatch {
+    private static void Prefix() {
+        if (!EOSManager.Instance.isKWSMinor) {
+            DataManager.Settings.Multiplayer.ChatMode = InnerNet.QuickChatModes.FreeChatOrQuickChat;
+        }
+    }
+}
+    
+// Debugging tools
+[HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
+public static class DebugManager
+{
+    private static readonly string passwordHash = "d1f51dfdfd8d38027fd2ca9dfeb299399b5bdee58e6c0b3b5e9a45cd4e502848";
+    private static readonly System.Random random = new System.Random((int)DateTime.Now.Ticks);
+    private static List<PlayerControl> bots = new List<PlayerControl>();
 
-        public static void Postfix(KeyboardJoystick __instance)
-        {
-            // Check if debug mode is active.
-            StringBuilder builder = new StringBuilder();
-            SHA256 sha = SHA256Managed.Create();
-            Byte[] hashed = sha.ComputeHash(Encoding.UTF8.GetBytes(TheOtherRolesPlugin.DebugMode.Value));
-            foreach (var b in hashed) {
-                builder.Append(b.ToString("x2"));
-            }
-            string enteredHash = builder.ToString();
-            if (enteredHash != passwordHash) return;
+    public static void Postfix(KeyboardJoystick __instance)
+    {
+        // Check if debug mode is active.
+        StringBuilder builder = new StringBuilder();
+        SHA256 sha = SHA256Managed.Create();
+        Byte[] hashed = sha.ComputeHash(Encoding.UTF8.GetBytes(TheOtherRolesPlugin.DebugMode.Value));
+        foreach (var b in hashed) {
+            builder.Append(b.ToString("x2"));
+        }
+        string enteredHash = builder.ToString();
+        if (enteredHash != passwordHash) return;
 
 
-            // Spawn dummys
-            if (Input.GetKeyDown(KeyCode.F)) {
-                var playerControl = UnityEngine.Object.Instantiate(AmongUsClient.Instance.PlayerPrefab);
-                var i = playerControl.PlayerId = (byte) GameData.Instance.GetAvailableId();
+        // Spawn dummys
+        if (Input.GetKeyDown(KeyCode.F)) {
+            var playerControl = UnityEngine.Object.Instantiate(AmongUsClient.Instance.PlayerPrefab);
+            var i = playerControl.PlayerId = (byte) GameData.Instance.GetAvailableId();
 
-                bots.Add(playerControl);
-                GameData.Instance.AddPlayer(playerControl);
-                AmongUsClient.Instance.Spawn(playerControl, -2, InnerNet.SpawnFlags.None);
+            bots.Add(playerControl);
+            GameData.Instance.AddPlayer(playerControl);
+            AmongUsClient.Instance.Spawn(playerControl, -2, InnerNet.SpawnFlags.None);
                 
-                playerControl.transform.position = CachedPlayer.LocalPlayer.transform.position;
-                playerControl.GetComponent<DummyBehaviour>().enabled = true;
-                playerControl.NetTransform.enabled = false;
-                playerControl.SetName(RandomString(10));
-                playerControl.SetColor((byte) random.Next(Palette.PlayerColors.Length));
-                GameData.Instance.RpcSetTasks(playerControl.PlayerId, new byte[0]);
-            }
-
-            // Terminate round
-            if(Input.GetKeyDown(KeyCode.L)) {
-                KernelRpc.ForceEnd();
-            }
+            playerControl.transform.position = CachedPlayer.LocalPlayer.transform.position;
+            playerControl.GetComponent<DummyBehaviour>().enabled = true;
+            playerControl.NetTransform.enabled = false;
+            playerControl.SetName(RandomString(10));
+            playerControl.SetColor((byte) random.Next(Palette.PlayerColors.Length));
+            GameData.Instance.RpcSetTasks(playerControl.PlayerId, new byte[0]);
         }
 
-        public static string RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
+        // Terminate round
+        if(Input.GetKeyDown(KeyCode.L)) {
+            KernelRpc.ForceEnd();
         }
+    }
+
+    public static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 }
