ceb7589791824de2d431e9b529956d6aa346c596
Eno9991 <marwane.rhaiem@gmail.com>
2023-05-22 22:30:06 +0200

    Temp fix

--- BetterOtherRoles/Modules/DynamicLobbies.cs
+++ BetterOtherRoles/Modules/DynamicLobbies.cs
@@ -3,24 +3,25 @@ using AmongUs.GameOptions;
 using HarmonyLib;
 using InnerNet;
 
-namespace BetterOtherRoles.Modules {
-    [HarmonyPatch]
-    public static class DynamicLobbies {
-        public static int LobbyLimit = 15;
-        [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.HostGame))]
-        public static class InnerNetClientHostPatch {
-            public static void Prefix(InnerNet.InnerNetClient __instance, [HarmonyArgument(0)] GameOptionsData settings) {
-                DataManager.Settings.Multiplayer.ChatMode = InnerNet.QuickChatModes.FreeChatOrQuickChat;
-            }
-            public static void Postfix(InnerNet.InnerNetClient __instance, [HarmonyArgument(0)] GameOptionsData settings) {
-                settings.MaxPlayers = DynamicLobbies.LobbyLimit;
-            }
+namespace BetterOtherRoles.Modules;
+/*
+[HarmonyPatch]
+public static class DynamicLobbies {
+    public static int LobbyLimit = 15;
+    [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.HostGame))]
+    public static class InnerNetClientHostPatch {
+        public static void Prefix(InnerNet.InnerNetClient __instance, [HarmonyArgument(0)] GameOptionsData settings) {
+            DataManager.Settings.Multiplayer.ChatMode = InnerNet.QuickChatModes.FreeChatOrQuickChat;
         }
-        [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.JoinGame))]
-        public static class InnerNetClientJoinPatch {
-            public static void Prefix(InnerNet.InnerNetClient __instance) {
-                DataManager.Settings.Multiplayer.ChatMode = InnerNet.QuickChatModes.FreeChatOrQuickChat;
-            }
+        public static void Postfix(InnerNet.InnerNetClient __instance, [HarmonyArgument(0)] GameOptionsData settings) {
+            settings.MaxPlayers = DynamicLobbies.LobbyLimit;
+        }
+    }
+    [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.JoinGame))]
+    public static class InnerNetClientJoinPatch {
+        public static void Prefix(InnerNet.InnerNetClient __instance) {
+            DataManager.Settings.Multiplayer.ChatMode = InnerNet.QuickChatModes.FreeChatOrQuickChat;
         }
     }
 }
+*/
